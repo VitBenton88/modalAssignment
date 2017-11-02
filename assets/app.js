d2d3e544cb0e379295314163a4472249f0f1a4ac
Vit Benton <vitbenton@Macintosh.local>
2017-11-01 20:31:00 -0400

    started modal styling/functionality

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+            // assign the modal
+            var modal = $('.modal');
+
+            // assign the button that opens the modal
+            var modalOpenBtn = $("#modalOpenBtn");
+
+            // assign the button that opens the modal
+            var modalCloseBtn = $(".close");
+
+            // When the user clicks on the open button, open the modal 
+            modalOpenBtn.click(function(){
+            	modal.css("display", "initial");
+            });
+
+            // When the user clicks on the close button, close the modal 
+            modalCloseBtn.click(function(){
+            	modal.css("display", "none");
+            });
+
+        });
