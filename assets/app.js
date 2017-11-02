c3a4a1a88c9db2edbf91816ed50724c8e4659706
Vit Benton <vitbenton@Macintosh.local>
2017-11-01 22:09:07 -0400

    prettified some code

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
-            // assign the modal
-            var modal = $('.modal');
+    // assign the modal
+    var modal = $('.modal');
 
-            // assign the button that opens the modal
-            var modalOpenBtn = $("#modalOpenBtn");
+    // assign the button that opens the modal
+    var modalOpenBtn = $("#modalOpenBtn");
 
-            // assign the button that opens the modal
-            var modalCloseBtn = $("#modalCloseBtn");
+    // assign the button that opens the modal
+    var modalCloseBtn = $("#modalCloseBtn");
 
-            // When the user clicks on the open button, open the modal 
-            modalOpenBtn.click(function(){
-            	modal.css("display", "initial");
-            });
+    // When the user clicks on the open button, open the modal 
+    modalOpenBtn.click(function() {
+        modal.css("display", "initial");
+    });
 
-            // When the user clicks on the close button, close the modal 
-            modalCloseBtn.click(function(){
-            	modal.css("display", "none");
-            });
+    // When the user clicks on the close button, close the modal 
+    modalCloseBtn.click(function() {
+        modal.css("display", "none");
+    });
 
-        });
+});
